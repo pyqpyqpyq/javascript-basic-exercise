81cb1b37ec1f89ce3c78e1e3515319da860855e4
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-10 12:32:31 +0800

    [pyqpyqpyq] feat:javascript-basic_exercise5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,15 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const ans = [];
+  if (array === undefined || array === null) { throw new Error('Flatten undefined or null array'); }
+  if (array.length === 0) { return []; }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) !== 'number') {
+      for (let j = 0; j < array[i].length; j += 1) {
+        ans.push(array[i][j]);
+      }
+    } else { ans.push(array[i]); }
+  }
+  return ans;
 }
